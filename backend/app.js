750f9070478686348194658de71f2bdf9d6f0e02
François <ledune.francois@hotmail.fr>
2022-07-07 21:09:05 +0200

    etapes 1, 2, 3 finies

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const bodyParser = ('body-parser');
-const mongoose = require('mongoose');
-
 const app = express();
-
-mongoose.connect('mongodb+srv://lastblaze:<mongo>@cluster0.qtsrj.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-app.use(express.json());
+const mongoose = require('mongoose');
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+const path = require('path');
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -19,33 +12,18 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Objet créé !'
-  });
-});
+app.use(express.json());
 
-app.use('/api/stuff', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-  res.status(200).json(stuff);
-});
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauce', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+mongoose.connect('mongodb+srv://lastblaze:mongo@cluster0.qtsrj.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 module.exports = app;
+
--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,32 @@ const http = require('http');
 const app = require('./app');
 
 const normalizePort = val => {
-  const port = parseInt(vanodl, 10);
+  //Transform la valeur PORT en chiffre
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
-    return val;
+    return val; // return @ 
   }
   if (port >= 0) {
     return port;
   }
   return false;
 };
+
 const port = normalizePort(process.env.PORT || '3000');
+
 app.set('port', port);
+
 
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
     throw error;
   }
   const address = server.address();
+
+  // const qql = affiramtion ? si vaire : ... : si faux: ....;
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+
   switch (error.code) {
     case 'EACCES':
       console.error(bind + ' requires elevated privileges.');
